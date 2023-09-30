f40994d475152b7b9038e697eb035079663fb01c
Алексей Суханов <furyseer@gmail.com>
2023-09-30 23:21:11 +0300

    hw_2 class fix

---
+++ hw_2/scripts/create_collection.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+
+sys.path.insert(0,"..")
+
+from staff.collector import Collector
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Скрипт для получения числа файлов в директории",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--path',
+                        '-p',
+                        type=str,
+                        default='/',
+                        help="Путь для старта. Пример: /home/{USERNAME}/Downloads/")
+    args = parser.parse_args()
+
+    cl = Collector(args.path)
+
+    cl.collect()
--- hw_2/staff/collector.py
+++ hw_2/staff/collector.py
@@ -5,6 +5,7 @@ import csv
 import os
 from os.path import join, getsize, islink, getmtime, getctime
 
+
 class Collector:
     """Make CSV with some data about all files in directory"""
 
@@ -14,40 +15,63 @@ class Collector:
                          'Create', 'Modifying')
         self._writer = callable
         self._dict_string = {}
-        self._full_paths = []
+        self._all_files_and_full_paths = []
+        self._filter_files_and_full_paths = []
         self._file = str()
         self._dirpath = str()
         self._filenames = str()
 
     def _symlink_filter(self):
         """Отбор путей, не являющихся символическими ссылками"""
-        self._full_paths = list(filter(lambda x: not islink(
-            x), [join(self._dirpath, self._file) for self._file in self._filenames]))
+        self._all_files_and_full_paths = [
+            (self._file, join(self._dirpath, self._file)) for self._file in self._filenames]
+        self._filter_files_and_full_paths = list(
+            filter(lambda x: not islink(x[1]), self._all_files_and_full_paths))
 
-        return self._full_paths
+        return self._filter_files_and_full_paths
 
-    def _scan_dir(self):
+    def _scan_dir_generator(self):
 
         self._filenames = self._symlink_filter()
 
         for self._file in self._filenames:
-            yield {'Name': self._file.split('/')[-1],
-                   'Full_path': self._file,
-                   'Size_bytes': getsize(self._file),
-                   'Create': datetime.fromtimestamp(getctime(self._file)).date(),
-                   'Modifying': datetime.fromtimestamp(getmtime(self._file)).date()
+            yield {'Name': self._file[0],
+                   'Full_path': self._file[1],
+                   'Size_bytes': getsize(self._file[1]),
+                   'Create': datetime.fromtimestamp(getctime(self._file[1])).date(),
+                   'Modifying': datetime.fromtimestamp(getmtime(self._file[1])).date()
                    }
+
+    def _write_dir_data(self):
+        if not os.path.exists('../data/index.csv'):
+            with open('../data/index.csv', 'w', encoding='utf-8') as self._file:
+                self._writer = csv.DictWriter(
+                    self._file, fieldnames=self._headers)
+                self._writer.writeheader()
+        else:
+            with open('../data/index.csv', 'a', encoding='utf-8') as self._file:
+                self._writer = csv.DictWriter(
+                    self._file, fieldnames=self._headers)
+                for self._dict_string in self._scan_dir_generator():
+                    self._writer.writerow(self._dict_string)
 
     def collect(self):
         """Colllect data from directories and make CSV file"""
+        if os.path.exists('../data/index.csv'):
+            os.remove("../data/index.csv")
         for self._dirpath, _, self._filenames in os.walk(self.path_from):
-            with open('index.csv', 'a', encoding='utf-8') as self._file:
-                self._writer = csv.DictWriter(self._file, fieldnames=self._headers)
-                self._writer.writeheader()
-                for self._dict_string in self._scan_dir():
-                    self._writer.writerow(self._dict_string)
+            self._write_dir_data()
 
 
-cl = Collector('/home/furyseer/Загрузки/')
+# cl = Collector('/')
 
-cl.collect()
+# import time
+# def prog_exe_time(func: callable) -> (float, any):
+#     """Возвращается время работы программы и результат работы функции"""
+#     start = time.time()
+#     func()
+#     end = time.time()
+#     return round(end-start, 2)
+
+# result = prog_exe_time(cl.collect)
+# print(f"Время выполнения программы : {result} сек.")
